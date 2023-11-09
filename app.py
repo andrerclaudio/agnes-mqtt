5b6f9a8c7deb2541b2af2f2dd8c91c91519ecf48
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2023-11-09 20:09:32 -0300

    Initial commit.

--- app.py
+++ app.py
@@ -1,73 +1,122 @@
-# Build-in modules
+"""
+Application in general.
+"""
+
 import logging
-import time
-from datetime import timedelta
-from threading import ThreadError, Thread
-
-# Added modules
-from pytictoc import TicToc
-
-# Project modules
-
-# Print in file
-# logging.basicConfig(filename='logs.log',
-#                     filemode='w',
-#                     level=logging.INFO,
-#                     format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
-#                     datefmt='%d/%b/%Y - %H:%M:%S')
-
-# Print in software terminal
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
-                    datefmt='%d/%b/%Y - %H:%M:%S')
-
-logger = logging.getLogger(__name__)
+import traceback
+import paho.mqtt.client as mqtt
 
 
-class ElapsedTime(object):
+# Define the MQTT broker information
+BROKER_ADDRESS = "mqtt.eclipseprojects.io"
+PORT = 1883
+TOPIC = "my_topic"
+CLIENT_ID = "my_client"
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
+
+
+class MQTTConnectionError(Exception):
     """
-    Measure the elapsed time between Tic and Toc
-    """
-    def __init__(self):
-        self.t = TicToc()
-        self.t.tic()
+    Custom exception for MQTT connection errors.
 
-    def elapsed(self):
-        _elapsed = self.t.tocvalue()
-        d = timedelta(seconds=_elapsed)
-        logger.info('< {} >'.format(d))
+    This exception is raised when there is a failure to establish a connection
+    with an MQTT broker.
 
+    Args:
+        message (str): An optional custom error message to describe the specific
+        connection issue. If not provided, a default message is used.
 
-class ThreadingProcessQueue(object):
-    """
-    The run() method will be started and it will run in the background
-    until the application exits.
+    Attributes:
+        message (str): The error message describing the connection issue.
+
+    Example:
+        You can raise this exception with a custom message when handling MQTT
+        connection errors to provide more context about the issue.
+
     """
 
-    def __init__(self, interval):
-        """
-        Constructor
-        """
-        self.interval = interval
-
-        thread = Thread(target=run, args=(self.interval,), name='Thread_name')
-        thread.daemon = True  # Daemonize thread
-        thread.start()  # Start the execution
+    def __init__(self, message="MQTT connection failed"):
+        self.message = message
+        super().__init__(self.message)
 
 
-def run(interval):
-    """ Method that runs forever """
-    while True:
-        try:
-            time.sleep(interval)
+def on_connect(client, userdata, flags, rc) -> None:
+    """
+    MQTT on_connect callback function.
 
-        except ThreadError as e:
-            logger.exception('{}'.format(e))
+    This function is called when the MQTT client successfully connects to the broker
+    or encounters an error during connection.
 
-        finally:
-            pass
+    Args:
+        client (paho.mqtt.client.Client): The MQTT client instance.
+        userdata (Any): User-defined data passed to the client.
+        flags (dict): Flags indicating specific MQTT connection flags.
+        rc (int): The connection result code.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If the MQTT connection fails (rc is not 0).
+
+    Example:
+        This function is typically set as the on_connect callback when configuring
+        an MQTT client. It handles the logic for successful and failed connections.
+
+    """
+
+    if rc == 0:
+
+        _ = client
+        _ = userdata
+        _ = flags
+
+        logging.info("Connected to MQTT broker")
+        client.subscribe(TOPIC)
+    else:
+        error_msg = f"Connection failed with code {rc}"
+        logging.error(error_msg)
+        raise MQTTConnectionError(error_msg)
+
+
+def on_message(client, userdata, message) -> None:
+    """
+    Parse the incoming messages.
+    """
+    
+    _ = client
+    _ = userdata
+    logging.info("Received message: %s", message.payload.decode())
+
+
+# MQTT client setup
+mqttc = mqtt.Client(CLIENT_ID)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
 
 
 def application():
     """" All application has its initialization from here """
-    logger.info('Main application is running!')
+    logging.info('Main application is running!')
+
+    try:
+
+        mqttc.connect(BROKER_ADDRESS, PORT)
+        mqttc.loop_start()
+
+        while True:
+            pass
+
+            # message = "message to publish!"
+            # mqttc.publish(TOPIC, message)
+            # logging.info("Published message: %s", message)
+
+    except mqtt as e:
+        logging.exception("An error occurred: %s", str(e), exc_info=False)
+        traceback.print_exc()
+
+    finally:
+        mqttc.disconnect()
+        logging.info("Disconnected from MQTT broker")
--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+"""
+This is just a simple implemetation of MQTT.
+"""
+
+
 # Import the main application
 from app import application
 
 
-def main():
+def main() -> None:
+    """
+    The main entry point for the application.
+
+    This function serves as the entry point for the application, calling the `application()`
+    function from the 'app' module.
+    It can be used to start the application and perform any necessary setup or configuration.
+
+    Returns:
+        None: This function does not return any value.
+
+    Example:
+        To run the application, simply call `main()` from your script.
+
+    """
     application()
-    return None
 
 
 if __name__ == "__main__":
